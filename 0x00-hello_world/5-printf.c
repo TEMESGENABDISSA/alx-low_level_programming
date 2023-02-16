e111aa3679b2674edbe8b61a186792728be6effc
TEMESGEN ABDISSA <temesgenabdissa2@gmail.com>
2023-02-16 11:52:43 -0800

    TASK 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * My hello, printf task
+ * Return: 0 (success)
+ */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+		return (0);
+
+}
+
