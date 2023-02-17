51ecefa1026031d51717642ee334b646d51d176c
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-02-17 13:54:03 -0800

    TASK  3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * return=always 0(success)
+ */
+int main(void){
+        char c ;
+        char d ;
+        c = 'a';
+        d = 'A';
+   while
+           ( c <= 'z'){
+          putchar(c);
+          c++ ;
+}
+  while
+          ( d <= 'Z'){
+         putchar(d);
+         d++ ;
+}
+        putchar('\n');
+
+        return(0);
+}
