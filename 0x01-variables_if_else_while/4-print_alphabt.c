d3c2d8921d07cdd3b4ca63b3002451be869e28a2
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-02-17 13:56:22 -0800

    TASK : 4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main-Entry point 
+ * Return : always 0(success)
+ */
+int main(void)
+{
+        char low , e , q ;
+        e = 'e' ;
+        q = 'q' ;
+ for ( low = 'a' ; low <= 'z' ; low++)
+{
+
+   if   ( low != e && low != q)
+           putchar(low);
+}
+    putchar ('\n');
+
+   return (0);
+   }
