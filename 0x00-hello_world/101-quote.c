05a046c0470e2bc464127c9191c43d952d976994
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-02-19 10:23:15 -0800

    Task 100-intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main - prints"and that piece of art is useful" - Dora Korpar, 2015-10-19",
+* followed by  a new line, to standard error 
+*return : always 1(success)
+*/
+int main(void)
+{
+     write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+return (1) ;
+}
