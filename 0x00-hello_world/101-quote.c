c2a834b89c64116c28e270f4ac1740258ef213f4
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-02-19 10:40:01 -0800

    TASK 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
-
 /**
- * main -Prints a text
- *
- * Return: Always 1 (Success)
+ * main - Entry point
+ * Return:1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-	return(1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
