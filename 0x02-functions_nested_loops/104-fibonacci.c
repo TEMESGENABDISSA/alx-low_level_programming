dab6ea63d2f3822dff5b1700844a874a2358921b
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-02-21 14:34:33 -0800

    TASK SIXTEEN 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,45 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * AUTHOR = This task is done by TEMESGEN ABDISSA
- * Description: computes and prints even  number < 4,000,000
- * 5 below 1024 (excluded), followed by a new line
- * Return: 0
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
+ * followed by a new line
+ * AUTHOR = TEMESGEN ABDISSA 
+ * Return: ALways 0 (Success)
  */
 int main(void)
 {
-	int i = 0;
-	int a = 0, b = 1, next = 0;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	while (next < 4000000)
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		next = a + b;
-		a = b;
-		b = next;
-		if (next % 2 == 0)
-			printf("%i\n", next);
-
-		i++;
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
+	printf("\n");
+
 	return (0);
 }
+
