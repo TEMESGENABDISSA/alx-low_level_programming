a07cdc3e6b85b25722c60ec6d93be80432ed9eca
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-02-21 15:04:46 -0800

    TASK THIRTEEN  101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,31 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - computes and prints the sum of all the multiples
- * of 3 or 5 below 1024 
+ * main - Lists all the natural numbers below 1024 (excluded)
+ *        that are multiples of 3 or 5.
  * AUTHOR = This task is done by TEMESGEN ABDISSA
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-unsigned long int sum3, sum5, sum;
+	int i, sum = 0;
 
-int i;
-sum3 = 0;
-sum5 = 0;
-sum = 0;
-for (i = 0; i < 1024; ++i)
-{
-if ((i % 3) == 0)
-{
-sum3 = sum3 + i;
-} else if ((i % 5) == 0)
-{
-sum5 = sum5 + i;
-}
-}
-sum = sum3 + sum5;
-printf("%lu\n", sum);
-return (0);
-}
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
 
+	printf("%d\n", sum);
+
+	return (0);
+}
