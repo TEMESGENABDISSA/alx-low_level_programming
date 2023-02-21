078a9e2041bb058037c9e6c04022e64bf7dd1398
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-02-21 13:56:16 -0800

    TASK FIFTEEN 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: computes and prints even  number < 4,000,000
+ * AUTHOR = This task is done by TEMESGEN ABDISSA
+ * 5 below 1024 (excluded), followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	int a = 0, b = 1, next = 0;
+
+	while (next < 4000000)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		if (next % 2 == 0)
+			printf("%i\n", next);
+
+		i++;
+	}
+	return (0);
+}
+
