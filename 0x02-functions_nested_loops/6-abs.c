1cfed8dd15d0840fc7fdf54b59ed27b8a943f163
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-02-21 13:05:02 -0800

    TASK SIX 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - returns absolute value of an int
+ * @n: integer from which to get an absolute value
+ * AUTHOR = This task is done by TEMESGEN ABDISSA
+ * Return: Void.
+ */
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
