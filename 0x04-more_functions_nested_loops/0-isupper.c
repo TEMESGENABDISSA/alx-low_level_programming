7e960be1dd0b4b0048c739907e43fe5b7d657cdf
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-02-23 09:52:50 -0800

    TASK 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - Check if paramter c represents an uppercase letter.
+ * @c: Int representing a character
+ * THIS task is done by TEMESGEN ABDISSA
+ * Return: Always 0.i
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
