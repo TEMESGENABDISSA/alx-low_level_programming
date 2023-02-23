11dae5af1bedf23d72ee6f4bb14723cb506cd201
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-02-23 11:18:11 -0800

    TASK 2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: integer argument
+ * @b: integer argument
+ * Task is done by temesgen abdissa
+ * Return: an integer
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
+
