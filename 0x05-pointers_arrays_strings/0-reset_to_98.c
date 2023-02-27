ea166a71c876927a7cdbb063eef5acea1b6bbd94
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-02-27 12:52:19 -0800

    Task 0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*  reset_to_98 - takes the pointer and updates the value
+*  @: int to check
+*  done by TEMESGEN ABDISSA
+*  Return: 0 is success
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
