e38d42b6fddd2110d6e539fc156a06f30c289ac2
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-02-27 13:04:06 -0800

    Task 0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
-/**
-*  reset_to_98 - takes the pointer and updates the value
-*  @: int to check
-*  done by TEMESGEN ABDISSA
-*  Return: 0 is success
-*/
 
+/**
+ * reset_to_98 - resets value of pointer to 98
+ * @n: pointer to reset to 98
+ * done by :Temesgen Abdissa
+ * Return: void
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
