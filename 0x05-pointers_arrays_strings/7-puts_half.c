762dfab31f1c424afacb505d1ecd2048cce892dd
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-02-27 13:38:57 -0800

    Task 7. Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - half of a string.
+ * @str: string to be printed.i
+ * done by Temesgen Abdissa
+ */
+void puts_half(char *str)
+{
+	int i = 0, len = 0, j;
+
+	while (str[i++])
+		len++;
+
+	if ((len % 2) == 0)
+		j = len / 2;
+
+	else
+		j = (len + 1) / 2;
+
+	for (i = j; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
+
