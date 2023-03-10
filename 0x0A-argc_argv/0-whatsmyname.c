32dea4be3275989bcbc273d448021274691a5760
TEMESGENABDISSA <temesgenabdissa2@gmail.com>
2023-03-10 07:48:58 -0800

    C - argc, argv Tasks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the argument.
+ * done by temesgen abdissa
+ * Return: Always 0.
+ */
+int main(int __attribute((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
