7055dab68fc6d1b0f4af2254f4064ad44b50f028
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-03-30 13:10:52 -0700

    Tasks depend on the C - Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,25 @@
-#include <stdio.h>
+#include  <stdio.h>
 #include "lists.h"
-
+#include <stdlib.h>
 /**
-* print_list - prints all the elements of a linked list
-* @h: pointer to the list_t list to print
-*
-* Return: the number of nodes printed
-*/
+ * print_list - finctions that print list
+ * @h: linked list header
+ *
+ * Return: return x
+ */
 size_t print_list(const list_t *h)
 {
-size_t s = 0;
-while (h)
-{
-if (!h->str)
-printf("[0] (nil)\n");
-else
-printf("[%u] %s\n", h->len, h->str);
-h = h->next;
-s++;
-}
-return (s);
-}
+	size_t x = 0;
 
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		x++;
+	}
+	return (x);
+
+}
