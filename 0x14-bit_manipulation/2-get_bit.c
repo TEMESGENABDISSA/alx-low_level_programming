cd235ad066c96d7dc7b0e77860c0ac3f1970ac0f
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-04-06 12:35:39 -0700

    Task 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- * get_bit - Gets the value of a bit at a given index.
- * @n: The bit.
- * @index: The index to get the value at - indices start at 0.
- * This task is done by Temesgen abdissa
- * Return: If an error occurs - -1.
- *         Otherwise - The value of bit at index.
+ * get_bit - returns the value of a bit at a given index
+ * @n: the unsigned long integer to retrieve the bit from
+ * @index: the index of the bit to retrieve, starting from 0
+ *
+ * Return: the value of the bit at index index, or -1 if an error occurred
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		/* Error: index out of range */
 		return (-1);
+	}
 
-	if ((n & (1 << index)) == 0)
-		return (0);
+	unsigned long int mask = 1UL << index;
 
-	return (1);
+	return ((n & mask) != 0);
 }
+
