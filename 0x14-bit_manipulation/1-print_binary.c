353d8f32afd2faea086f037ad17151d4f30fea2b
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-04-06 06:12:11 -0700

    Task 1.1. print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,24 +4,10 @@
  * print_binary - Prints the binary representation of a number.
  * @n: The number to be printed in binary.
  */
-void print_binary(unsigned long int n)
-{
-    if (n == 0) {
-        _putchar('0');
-        return;
-    }
-
-    int binary_digits[sizeof(n) * 8];
-    int i = 0;
-
-    while (n > 0) {
-        binary_digits[i++] = n % 2;
-        n = n / 2;
-    }
-
-    for (int j = i - 1; j >= 0; j--) {
-        _putchar(binary_digits[j] + '0');
+void print_binary(unsigned long int n) {
+    for(int i = sizeof(unsigned long int) * 8 - 1; i >= 0; i--) {
+        unsigned long int mask = 1UL << i;
+        _putchar((n & mask) ? '1' : '0');
     }
 }
-
 
