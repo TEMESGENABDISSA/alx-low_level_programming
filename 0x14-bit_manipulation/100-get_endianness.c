fa54ed9349c2e1a0b89421480d26f03dc9002f9f
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-04-06 10:18:08 -0700

    Task get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
-/**
-
-    get_endianness - Checks the endianness.
-    Return: If big-endian - 0.
-If little-endian - 1.
-*/
 int get_endianness(void)
 {
 int x = 1;
 char *c = (char *)&x;
+
+
+
 return (int)(*c);
+
 }
