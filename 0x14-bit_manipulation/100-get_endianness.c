26d994fccc578ca89b7d11b9da4b1e6cd59ece40
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-04-06 09:38:55 -0700

    Task get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -6,13 +6,12 @@
  * Return: If big-endian - 0.
  *         If little-endian - 1.
  */
-int get_endianness(void)
-{
-	int num = 1;
-	char *endian = (char *)&num;
-
-	if (*endian == 1)
-		return (1);
-
-	return (0);
+int get_endianness(void) {
+    unsigned int num = 1;
+    char* ptr = (char*)&num;
+    if (*ptr == 1) 
+        return 1;
+    } else {
+        return 0;
+    }
 }
