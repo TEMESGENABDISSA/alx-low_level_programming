7173af7dee5902430d76bdb0c84205baab51ff0b
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-04-10 13:13:37 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,22 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/*
-
-    File: main.h
-    Auth: Temesgen abdissa
-    Desc: Header file containing prototypes for all functions
-
-*/
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-/* Function prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* MAIN_H */
+#endif
