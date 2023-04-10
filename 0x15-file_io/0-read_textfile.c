9e6a9772142f444978ce8823f7a9bbdfabd949a5
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-04-10 11:43:33 -0700

    Task 0-read_text_to file.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,42 +12,42 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t n_read, n_written;
-	char *buf;
+    int fd;
+    ssize_t n_read, n_written;
+    char *buf;
 
-	if (filename == NULL)
-		return (0);
+    if (filename == NULL)
+        return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
 
-	buf = malloc(sizeof(char) * (letters + 1));
-	if (buf == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+    buf = malloc(sizeof(char) * (letters + 1));
+    if (buf == NULL)
+    {
+        close(fd);
+        return (0);
+    }
 
-	n_read = read(fd, buf, letters);
-	if (n_read == -1)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
+    n_read = read(fd, buf, letters);
+    if (n_read == -1)
+    {
+        free(buf);
+        close(fd);
+        return (0);
+    }
 
-	n_written = write(STDOUT_FILENO, buf, n_read);
-	if (n_written == -1 || n_written != n_read)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
+    n_written = write(STDOUT_FILENO, buf, n_read);
+    if (n_written == -1 || n_written != n_read)
+    {
+        free(buf);
+        close(fd);
+        return (0);
+    }
 
-	free(buf);
-	close(fd);
-	return (n_written);
+    free(buf);
+    close(fd);
+    return (n_written);
 }
 
