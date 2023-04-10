e00674df0703e0b43c80077653325e74c79689ab
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-04-10 11:16:58 -0700

    Task 0-read_text_to file.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
@@ -9,36 +12,42 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd, bytes_read, bytes_written;
-    char *buffer;
+	int fd;
+	ssize_t n_read, n_written;
+	char *buf;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
+	buf = malloc(sizeof(char) * (letters + 1));
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-    bytes_read = read(fd, buffer, letters);
-    if (bytes_read == -1)
-    {
-        free(buffer);
-        return (0);
-    }
+	n_read = read(fd, buf, letters);
+	if (n_read == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    free(buffer);
+	n_written = write(STDOUT_FILENO, buf, n_read);
+	if (n_written == -1 || n_written != n_read)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 
-    if (bytes_written == -1 || bytes_written != bytes_read)
-        return (0);
-
-    if (close(fd) == -1)
-        return (0);
-
-    return (bytes_read);
+	free(buf);
+	close(fd);
+	return (n_written);
 }
 
