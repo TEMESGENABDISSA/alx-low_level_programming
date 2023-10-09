befc4161a24e44089edf08be1643e1663affb68b
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-10-09 05:30:37 -0700

    Jump search algorithm

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+#include <math.h>
+/**
+ * jump_search - searche for a values in  array of
+ * integers using the Jump_ search_ algorithm
+ * done by temesgen abdissa
+ * @array: input the  array
+ * @size: size of the array
+ * @value: the  value to search int
+ * Return:  the index of the numb
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int index, m, k, prev;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	m = (int)sqrt((double)size);
+	k = 0;
+	prev = index = 0;
+
+	do {
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+			return (index);
+		k++;
+		prev = index;
+		index = k * m;
+	} while (index < (int)size && array[index] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+	for (; prev <= index && prev < (int)size; prev++)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
+	}
+
+	return (-1);
+}
