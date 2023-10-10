a3c9cb803d6a0a0c723ffa9d44cc8fa4479c6788
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-10-10 04:54:53 -0700

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,11 +1,11 @@
 #include "search_algos.h"
 /**
- * linear_search - search for a value in an array 
- * @array: the  array of integer
- * @size: a size of th  array
- * @value: a  value that should be found
+ * linear_search - searches for a value in an array (linear search)
+ * @array:  the array of integers
+ * @size: the  size of array
+ * @value: A  value that should be found
  * done by temesgen abdissa
- * Return: the  index of value search  -1 if it doesn't exist
+ * Return: index of value searched or -1 if it doesn't exist
  */
 int linear_search(int *array, size_t size, int value)
 {
